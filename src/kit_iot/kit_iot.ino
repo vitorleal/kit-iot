c998d4feaee7e52543a6e976092aa7107c59ae0c
Vitor Leal <vitorleal1@gmail.com>
2014-01-07 09:29:43 -0200

    fixed spaces

--- src/kit_iot/kit_iot.ino
+++ src/kit_iot/kit_iot.ino
@@ -19,13 +19,13 @@ void setup() {
 void loop() {
   while (Serial.available() > 0) {
     char x = Serial.read();
-    
+
     if (x == '!') {
       index = 0;      // start
-      
+
     } else if (x == '.') {
       process(); // end
-      
+
     } else {
       messageBuffer[index++] = x;
     }
@@ -40,35 +40,35 @@ void process() {
 
   strncpy(cmd, messageBuffer, 2);
   cmd[2] = '\0';
-  
+
   strncpy(pin, messageBuffer + 2, 2);
   pin[2] = '\0';
 
   if (atoi(cmd) > 90) {
     strncpy(val, messageBuffer + 4, 2);
     val[2] = '\0';
-    
+
     strncpy(aux, messageBuffer + 6, 3);
     aux[3] = '\0';
-    
+
   } else {
     strncpy(val, messageBuffer + 4, 3);
     val[3] = '\0';
-    
+
     strncpy(aux, messageBuffer + 7, 3);
     aux[3] = '\0';
   }
-  
+
   Debug(messageBuffer);
-  
+
   int cmdid = atoi(cmd);
 
   switch (cmdid) {
-    case 1 :  dw(pin,val);     break;
-    case 2 :  dr(pin,val);     break;
-    case 3 :  aw(pin,val);     break;
-    case 4 :  ar(pin,val);     break;
-    case 5 :  dht11Read(pin);  break;
+    case 1 : dw(pin,val);     break;
+    case 2 : dr(pin,val);     break;
+    case 3 : aw(pin,val);     break;
+    case 4 : ar(pin,val);     break;
+    case 5 : dht11Read(pin);  break;
     case 99: toggleDebug(val); break;
     default:                   break;
   }
@@ -81,7 +81,7 @@ void toggleDebug(char *val) {
   if (atoi(val) == 0) {
     debug = false;
     Serial.println("goodbye");
-    
+
   } else {
     debug = true;
     Serial.println("hello");
@@ -93,22 +93,22 @@ void toggleDebug(char *val) {
  */
 void ar(char *pin, char *val) {
   Debug("ar");
-  
+
   int p = getPin(pin);
-  
+
   if(p == -1) {
     Debug("badpin");
     return;
   }
-  
+
   pinMode(p, INPUT);
   int rval = analogRead(p);
-  
+
   Serial.println(pin);
   if (pin == "A0") {
     Serial.println("yeahh");
   }
-  
+
   char m[8];
   sprintf(m, "%s::%03d", pin, rval);
   Serial.println(m);
@@ -120,15 +120,15 @@ void ar(char *pin, char *val) {
  */
 void aw(char *pin, char *val) {
   Debug("aw");
-  
+
   int p = getPin(pin);
   pinMode(p, OUTPUT);
-  
+
   if(p == -1) {
     Debug("badpin");
     return;
   }
-  
+
   analogWrite(p,atoi(val));
 }
 
@@ -138,19 +138,19 @@ void aw(char *pin, char *val) {
  */
 void dw(char *pin, char *val) {
   Debug("dw");
-  
+
   int p = getPin(pin);
-  
-  if(p == -1) { 
+
+  if(p == -1) {
     Debug("badpin");
-    return; 
+    return;
   }
-  
+
   pinMode(p, OUTPUT);
-  
+
   if (atoi(val) == 0) {
     digitalWrite(p, LOW);
-    
+
   } else {
     digitalWrite(p, HIGH);
   }
@@ -161,17 +161,17 @@ void dw(char *pin, char *val) {
  */
 void dr(char *pin, char *val) {
   Debug("dr");
-  
+
   int p = getPin(pin);
-  
-  if(p == -1) { 
+
+  if(p == -1) {
     Debug("badpin");
-    return; 
+    return;
   }
-  
+
   pinMode(p, INPUT);
   int oraw = digitalRead(p);
-  
+
   char m[7];
   sprintf(m, "%02d::%02d", p,oraw);
   Serial.println(m);
@@ -183,21 +183,21 @@ void dr(char *pin, char *val) {
  */
 int getPin(char *pin) {
   int ret = -1;
-  
+
   if(pin[0] == 'A' || pin[0] == 'a') {
     switch(pin[1]) {
-      case '0':  ret = A0; break;
-      case '1':  ret = A1; break;
-      case '2':  ret = A2; break;
-      case '3':  ret = A3; break;
-      case '4':  ret = A4; break;
-      case '5':  ret = A5; break;
-      default:             break;
+      case '0': ret = A0; break;
+      case '1': ret = A1; break;
+      case '2': ret = A2; break;
+      case '3': ret = A3; break;
+      case '4': ret = A4; break;
+      case '5': ret = A5; break;
+      default:            break;
     }
-    
+
   } else {
     ret = atoi(pin);
-    
+
     if(ret == 0 && (pin[0] != '0' || pin[1] != '0')) {
       ret = -1;
     }
@@ -212,14 +212,14 @@ int getPin(char *pin) {
 void dht11Read(char *pin) {
   Debug("dht11Read");
   int p = getPin(pin);
-  
-  if(p == -1) { 
+
+  if(p == -1) {
     Debug("badpin");
-    return; 
+    return;
   }
-  
+
   int chk = DHT11.read(atoi(pin));
-  
+
   char m[7];
   sprintf(m, "%s::%d::%d", pin, DHT11.temperature, DHT11.humidity);
   Serial.println(m);
