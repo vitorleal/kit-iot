eca153da6fe99782655b96ed02da06741ebeae71
Vitor Leal <vitorleal1@gmail.com>
2014-02-14 11:42:20 +0000

    added insight (analytics) to the app

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var libKit = require('./lib/kit-iot'),
-    KitIoT = new libKit();
+var libKit  = require('./lib/kit-iot'),
+    KitIoT  = new libKit(),
+    pkg     = require('./package.json'),
+    Insight = require('insight'),
+    insight = new Insight({
+      trackingCode  : 'UA-5427757-50',
+      packageName   : pkg.name,
+      packageVersion: pkg.version
+    });
 
 KitIoT.connect();
 KitIoT.start();
+insight.track('init');
